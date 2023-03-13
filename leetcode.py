24d5ae856e1319d2af2b5862ff87ea663455b2a9
Sheldon Chen <xidchen.work@gmail.com>
2023-03-13 13:53:15 +0800

    Update leetcode.py by adding problem 21

--- leetcode.py
+++ leetcode.py
@@ -312,6 +312,21 @@ class Leetcode:
                 stack.pop()
         return stack == []
 
+    # 21: /problems/merge-two-sorted-lists/
+    @staticmethod
+    def merge_two_lists(l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = temp = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        temp.next = l1 or l2
+        return dummy.next
+
     # 22: /problems/generate-parentheses/
     @staticmethod
     def generate_parenthesis(n: int) -> [str]:
