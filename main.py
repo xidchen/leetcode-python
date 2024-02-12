cee7d1c4a0e22fadb32b20324e31fec61f1b8040
Sheldon Chen <xidchen.work@gmail.com>
2024-02-12 20:30:15 +0800

    Rename test.py to main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from leetcode import Leetcode
+
+
+if __name__ == '__main__':
+    lc = Leetcode()
+    print(f"Problem 1: {lc.two_sum(nums=[2, 7, 11, 15], target=9)}")
+    print(f"Problem 3: {lc.length_of_longest_substring('abcdefabc')}")
+    print(f"Problem 4: {lc.find_median_sorted_arrays(a=[1, 2], b=[3, 4])}")
--- test.py
+++
@@ -1,6 +0,0 @@
-from leetcode import Leetcode
-
-
-if __name__ == '__main__':
-    lc = Leetcode()
-    print(lc.find_median_sorted_arrays(a=[3, 5, 8, 10], b=[12, 14, 15, 16]))
