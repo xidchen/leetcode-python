3ae5633febc7095494150574e3fee3b16a9f8a37
Sheldon Chen <xidchen.work@gmail.com>
2024-02-24 21:49:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from leetcode import Leetcode
 
 if __name__ == '__main__':
     lc = Leetcode()
-    print(f"Problem 1: {lc.two_sum(nums=[2, 7, 11, 15], target=9)}")
-    print(f"Problem 3: {lc.length_of_longest_substring('abcdefabc')}")
-    print(f"Problem 4: {lc.find_median_sorted_arrays(a=[1, 2], b=[3, 4])}")
+    print(f"Two sums: {lc.two_sum(nums=[2, 7, 11, 15], target=9)}")
+    print(f"Length of longest substring: {lc.length_of_longest_substring('abcdefabc')}")
+    print(f"Median of sorted arrays: {lc.find_median_sorted_arrays(a=[1, 2], b=[3, 4])}")
+    print(f"Longest palindrome: {lc.longest_palindrome(s='babad')}")
